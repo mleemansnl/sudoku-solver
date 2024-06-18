22f3045d35bae80a19f70113f370e7eedc9f5d7c
Maikel Leemans <maikel.leemans@gmail.com>
2024-06-18 14:17:43 +0000

    Updated java version, updated test case to use Java Text Block

--- src/java/com/sudokusolver/main/SudokuProcessorTest.java
+++ src/java/com/sudokusolver/main/SudokuProcessorTest.java
@@ -15,19 +15,21 @@ public class SudokuProcessorTest {
      */
     @Test
     void testSmallSudoku() {
-        StringReader input = new StringReader("4 _ _ 1\n" + //
-                "_ 1 3 _\n" + //
-                "_ 4 1 _\n" + //
-                "1 _ _ 3");
+        StringReader input = new StringReader("""
+                4 _ _ 1
+                _ 1 3 _
+                _ 4 1 _
+                1 _ _ 3""");
         StringWriter output = new StringWriter();
 
         boolean result = SudokuProcessor.processSudoku(input, output);
 
         assertTrue(result);
-        assertEquals("4 3 2 1 \n" + //
-                "2 1 3 4 \n" + //
-                "3 4 1 2 \n" + //
-                "1 2 4 3 \n" + //
-                "", output.toString());
+        assertEquals("""
+                4 3 2 1\s
+                2 1 3 4\s
+                3 4 1 2\s
+                1 2 4 3\s
+                """, output.toString());
     }
 }
